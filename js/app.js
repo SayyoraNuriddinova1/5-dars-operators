a57a52cdb7f5fc0a45a50992a15f35b3f1872c2a
Sayyora <sayyorachalabayeva94@gmail.com>
2023-10-24 20:55:19 +0500

    5dars

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let age = prompt("Yoshingiz nechida ekanligin ayting men sizni nechinchi yil ekanligingizni va 3 yildan keyin nechi yoshga to'lishingizni aytaman?");
+alert(2023-age);
+let answer=alert("3"+ " "+ "yildan" +" "+ "so'ng"+ " "+( Number(age)+3)+" "+"yoshga"+" "+"to'lasiz!" ); 
+let age1 = prompt("nechi yoshga kirishni istaysiz?");  
+let answer2 =alert("yana"+ " "+(age1-age) +" "+  "yil" + " "+ "qoldi.");  
